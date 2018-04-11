9513098ee2160e2fca2b5e9973be515bec353378
wujiaming <wujiaming@iplas.com.cn>
2018-04-11 14:56:07 +0800

    build.gradle

--- service/src/main/java/com/service/demo/ServiceApplication.java
+++ service/src/main/java/com/service/demo/ServiceApplication.java
@@ -3,7 +3,6 @@ package com.service.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
