9312c705dbe8e340e7f5f9855ab84bc53cce54d5
wujiaming <wujiaming@iplas.com.cn>
2018-04-11 15:18:00 +0800

    build.gradle

---
+++ client/src/main/java/com/client/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.client.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam String id) {
+        return restTemplate.getForObject("http://service-a/hi?id=" + id, String.class);
+    }
+}
